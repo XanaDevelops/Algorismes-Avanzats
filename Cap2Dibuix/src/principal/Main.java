1c27ed30377255a618c48a7ba8f9db42fe7974d0
Titrit <khaoulaberrada395@gmail.com>
2025-03-17 01:44:46 +0100

    tromins de fons

--- Cap2Dibuix/src/principal/Main.java
+++ Cap2Dibuix/src/principal/Main.java
@@ -1,8 +1,21 @@
 package principal;
 
+import model.TrominoSolver;
+
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+
 public class Main implements Comunicar {
+    static BufferedImage image;
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+        TrominoSolver solver = new TrominoSolver(2);
+        frame.add(solver);
+        frame.setVisible(true);
     }
 
     /**
