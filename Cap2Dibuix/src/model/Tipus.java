8ffbe67b1bedf2b886f49a9624b7897a47c90eb3
Ti-trit <khaoulaberrada395@gmail.com>
2025-03-22 18:07:26 +0100

    adptacions

--- Cap2Dibuix/src/model/Tipus.java
+++ Cap2Dibuix/src/model/Tipus.java
@@ -4,4 +4,5 @@ public enum Tipus {
     TROMINO,
     TRIANGLE,
     QUADRAT,
+    TREE
 }
--- Cap2Dibuix/src/model/solvers/TreeSolver.java
+++ Cap2Dibuix/src/model/solvers/TreeSolver.java
@@ -1,6 +1,7 @@
 package model.solvers;
 
 import model.Dades;
+import model.Tipus;
 import principal.*;
 
 import java.awt.*;
@@ -11,31 +12,34 @@ public class TreeSolver implements Runnable, Comunicar {
     private Dades data;
     private Main main;
     Graphics2D g;
+    private boolean stop;
 
     public TreeSolver(Main main, Dades data) {
         this.main = main;
         this.data = data;
 
-        int width = 1024;
-        int height = 1024;
-        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        data.setTipus(Tipus.TREE);
+        int size = 1024;
+        image = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
         g = image.createGraphics();
         g.setColor(Color.WHITE);
-        g.fillRect(0, 0, width, height);
+        g.fillRect(0, 0, size, size);
 
     }
 
     private void generarArbol(Graphics2D g, int x1, int y1, double angle, int p) {
-        if (p == 0) return;
+       if(!stop) {
+           if (p == 0) return;
 
-        int x2 = x1 + (int) (Math.cos(Math.toRadians(angle)) * 25 );
-        int y2 = y1 + (int) (Math.sin(Math.toRadians(angle)) * 25);
-        g.setColor(Color.RED);
+           int x2 = x1 + (int) (Math.cos(Math.toRadians(angle)) * 25 );
+           int y2 = y1 + (int) (Math.sin(Math.toRadians(angle)) * 25);
+           g.setColor(Color.RED);
 
-        g.drawLine(x1, y1, x2, y2);
+           g.drawLine(x1, y1, x2, y2);
 
-        generarArbol(g, x2, y2, angle - 20, p - 1);
-        generarArbol(g, x2, y2, angle + 20, p - 1);
+           generarArbol(g, x2, y2, angle - 20, p - 1);
+           generarArbol(g, x2, y2, angle + 20, p - 1);
+       }
     }
 
     public BufferedImage getImage() {
@@ -44,15 +48,36 @@ public class TreeSolver implements Runnable, Comunicar {
 
     @Override
     public void run() {
-        generarArbol(g, image.getHeight()/2, image.getHeight()-300, -90, data.getProfunditat());
+        stop = false;
+        //suposant aqui, q
+        double tempsEsperat = data.getConstantMultiplicativa()* Math.pow(2,data.getProfunditat());
+        main.comunicar("tempsEsperat "+ tempsEsperat);//??
 
+        long time = System.nanoTime();
+        generarArbol(g, image.getHeight()/2, image.getHeight()-300, -90, data.getProfunditat());
+        main.comunicar("aturar");
+
+        time = System.nanoTime() - time;
+        System.out.println("Temps real " + time + " segons");
+        main.comunicar("tempsReal "+ time);
         g.dispose();
+        if (!stop) {
+            aturar();
+        }
 
     }
 
     @Override
     public void comunicar(String s) {
+        switch (s){
+            case "aturar":
+                System.err.println("ATURAR carpeta sierpinski");
+                break;
+        }
 
+    }
+    private void aturar() {
+        stop = true;
     }
 
 
