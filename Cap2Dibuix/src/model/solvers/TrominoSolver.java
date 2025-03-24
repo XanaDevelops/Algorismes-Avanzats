cbe421239cdeeb2af8198105f4b4f08bb1d6cf1e
XanaDevelops <danielgv.space@gmail.com>
2025-03-24 11:01:59 +0100

    evita excepció al canviar entre modes de dibuix

--- Cap2Dibuix/src/model/solvers/TrominoSolver.java
+++ Cap2Dibuix/src/model/solvers/TrominoSolver.java
@@ -68,7 +68,9 @@ public class TrominoSolver extends RecursiveSolver implements  Comunicar {
             } else {
                 // Troba la posició del forat en el subtauler
                 int[] forat = trobarForat(topx, topy, mida);
-
+                if (forat == null) {
+                    return;
+                }
                 // Utilitzem l'enum Mode per determinar la ubicació del forat i procedir segons correspongui
                 Mode mode = determinarMode(forat[0], forat[1], topx, topy, mida);
 
@@ -92,6 +94,7 @@ public class TrominoSolver extends RecursiveSolver implements  Comunicar {
         int[] forat = new int[2];
         for (int x = topx; x < topx + mida; x++) {
             for (int y = topy; y < topy + mida; y++) {
+                if (data.getTauler() == null) return null;
                 if (data.getValor(x, y) != 0) {
                     forat[0] = x;
                     forat[1] = y;
@@ -148,6 +151,7 @@ public class TrominoSolver extends RecursiveSolver implements  Comunicar {
     private synchronized void omplirTromino(int topx, int topy, int mida) {
         for (int i = 0; i < mida; i++) {
             for (int j = 0; j < mida; j++) {
+                if(data.getTauler() == null) return;
                 if (data.getValor(topx + i, topy + j) == 0) {
                     data.setValor(topx + i, topy + j, numActual);
                     p.comunicar("pintar");
