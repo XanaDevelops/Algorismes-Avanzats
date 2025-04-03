eeab0b2e44348ef0c3001cc68e705fce88811918
JoFeF08 <135724351+JoFeF08@users.noreply.github.com>
2025-04-03 18:06:34 +0200

    Calcul parella propera amb força bruta

---
+++ Cap3DividirVencer/src/model/calculs/ParellaPropera_fb.java
@@ -0,0 +1,34 @@
+package model.calculs;
+
+import model.Dades;
+import model.punts.Punt;
+
+import java.util.List;
+
+public class ParellaPropera_fb extends Calcul {
+
+        public ParellaPropera_fb(List<Punt> punts,  Dades dades) {
+            super(punts, dades);
+        }
+
+        @Override
+        public void run() {
+                double min = Double.MAX_VALUE;
+                Punt p1 = null, p2 = null;
+                long t = System.nanoTime();
+                for (int i = 0; i < punts.size(); i++) {
+                        for (int j = i + 1; j < punts.size(); j++) {
+                                double d = punts.get(i).distancia(punts.get(j));
+                                if (d < min) {
+                                        min = d;
+                                        p1 = punts.get(i);
+                                        p2 = punts.get(j);
+                                }
+                        }
+                }
+                t = System.nanoTime() - t;
+                dades.addForcaBruta(p1, p2, min, t, "min");
+        }
+
+
+}
