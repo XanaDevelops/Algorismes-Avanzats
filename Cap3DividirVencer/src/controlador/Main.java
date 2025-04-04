f300821f8b0b8e2765cfe047969f77de5c0de74a
Ti-trit <khaoulaberrada395@gmail.com>
2025-04-04 22:03:49 +0200

    plantilla principal

--- Cap3DividirVencer/src/controlador/Main.java
+++ Cap3DividirVencer/src/controlador/Main.java
@@ -1,34 +1,97 @@
 package controlador;
 
 
+import model.Dades;
 import model.punts.Punt;
 import model.punts.Punt2D;
+import vista.Finestra;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main implements Comunicar {
-
+    Comunicar finestra;
+    Dades dades;
     private List<Punt> punts;
+
+    private ArrayList<Comunicar> processos = null;
+
+    private final ExecutorService executor = Executors.newFixedThreadPool(16);
 
     public static void main(String[] args) {
         (new Main()).init();
     }
 
     private void init() {
+        dades = new Dades();
         punts = new ArrayList<>();
+
+        executor.execute(() -> {
+            finestra = new Finestra(this, dades);
+        });
     }
 
     @Override
     public void comunicar(String s) {
-        if (s.startsWith("generar")) {
-            String[] res = s.split(":");
-            int num = Integer.parseInt(res[1]);
-            Random r = new Random();
-            punts.clear();
-            for (int i = 0; i < num; i++)
-                punts.add(new Punt2D(r.nextInt() * 600, r.nextInt() * 500));
+        String[] parts = s.split(":");
+        switch (parts[0]) {
+
+            case "generar":
+                String[] res = s.split(":");
+                int num = Integer.parseInt(res[1]);
+                Random r = new Random();
+                punts.clear();
+                for (int i = 0; i < num; i++)
+                    punts.add(new Punt2D(r.nextInt() * 600, r.nextInt() * 500));
+
+                System.out.println(Arrays.toString(punts.toArray()));
+                finestra.comunicar("dibuixPunts");
+                break;
+            case "classic":
+                break;
+
+            case "optimitzat":break;
+
+            case "aturar":
+                for (Comunicar proces : processos) {
+                    proces.comunicar("aturar");
+                }
+                break;
+            case "borar":
+                this.comunicar("aturar");
+               //esborrar els punts
+
+                finestra.comunicar("pintar");
+                break;
         }
+
     }
+
+    public Dades getDades() {
+        return dades;
+    }
+
+    public void setDades(Dades dades) {
+        this.dades = dades;
+    }
+    private void executar(Class<? extends Comunicar> clase, int profunditat) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        for (Comunicar enmarxa : processos) {
+            enmarxa.comunicar("aturar");
+        }
+
+        processos.clear();
+
+
+        Comunicar proces = (Comunicar) clase.getConstructor(Main.class, Dades.class).newInstance(this, dades);
+        processos.add(proces);
+        executor.execute((Runnable) proces);
+    }
+
+
+
 }
---
+++ Cap3DividirVencer/src/vista/Finestra.java
@@ -0,0 +1,98 @@
+package vista;
+
+import controlador.Comunicar;
+import model.Dades;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
+public class Finestra extends JFrame implements Comunicar {
+    public static final String ATURAR = "aturar";
+    public static final String ARRANCAR = "arrancar";
+    JComboBox<Integer> nPunts;
+    Comunicar comunicar;
+   Dades data;
+    JComboBox<String> algorime;
+
+
+    JPanel panellBotons;
+   public Finestra(Comunicar comunicar, Dades data) {
+       super();
+       this.comunicar = comunicar;
+       this.data = data;
+       this.setTitle("Distàncies a un nuvol de punts");
+       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       setResizable(false);
+       this.setLayout(new BorderLayout());
+       this.setPreferredSize(new Dimension(900, 900));
+
+       panellBotons = new JPanel();
+        nPunts = generateNPunts();
+
+       panellBotons.add(new JLabel("Nombre de Punts"));
+
+       panellBotons.add(nPunts);
+       ((JButton)panellBotons.add(new JButton(ATURAR))).addActionListener(e ->{
+              comunicar.comunicar(ATURAR);});
+
+       ((JButton)panellBotons.add(new JButton(ARRANCAR))).addActionListener(e ->{
+           comunicar.comunicar(ARRANCAR);
+       });
+
+       algorime = generateComBox();
+       panellBotons.add(algorime);
+      this.add(panellBotons, BorderLayout.NORTH);
+//      this.add(Eixos);
+
+
+      this.pack();
+      this.setLocationRelativeTo(null);
+      this.setVisible(true);
+
+   }
+
+    private JComboBox<String> generateComBox() {
+       JComboBox<String> comboBox = new JComboBox<>();
+       comboBox.addItem("Clàssic O(N)");
+       comboBox.addItem("Optimitzat O(N·log N)");
+
+       comboBox.setSelectedIndex(0); //default a classic
+        comboBox.addActionListener(e ->{
+            switch (comboBox.getSelectedIndex()) {
+            //per ara
+                case 0:
+                    comunicar.comunicar("classic");
+                    break;
+
+                case 1:
+                    comunicar.comunicar("optimitzat");
+                    break;
+
+
+
+            }
+        });
+
+       return comboBox;
+
+    }
+
+    private JComboBox<Integer> generateNPunts() {
+       JComboBox<Integer> comboBox = new JComboBox<>();
+       comboBox.addItem(100);
+       comboBox.addItem(500);
+       comboBox.addItem(1000);
+       comboBox.addItem(2000);
+       comboBox.addItem(5000);
+        comboBox.addItem(10000);
+
+        comboBox.addActionListener(e -> comunicar.comunicar("generar:" + comboBox.getSelectedItem()));
+       return comboBox;
+    }
+
+    @Override
+    public void comunicar(String s) {
+
+    }
+}
