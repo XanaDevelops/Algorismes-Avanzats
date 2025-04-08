d84b1b386e62a4bfb34e8a5b47f4695e9d806a26
Ti-trit <khaoulaberrada395@gmail.com>
2025-04-08 17:25:19 +0200

    moure list<Punts> a Dades

--- Cap3DividirVencer/src/model/calculs/Calcul.java
+++ Cap3DividirVencer/src/model/calculs/Calcul.java
@@ -3,15 +3,16 @@ package model.calculs;
 import model.Dades;
 import model.punts.Punt;
 
+import java.util.ArrayList;
 import java.util.List;
 
 abstract class Calcul implements Runnable {
 
-    protected final List<Punt> punts;
-    protected Dades dades;
 
-    public Calcul(List<Punt> punts, Dades dades) {
-        this.punts = punts;
+    protected Dades dades;
+    protected List<Punt> punts;
+    public Calcul( Dades dades) {
         this.dades = dades;
+        this.punts = dades.getPunts();
     }
 }
