2c3503ba50a4e77c734867350cee1e8c099909e9
Daniel GV <danielgv.space@gmail.com>
2025-04-16 20:07:07 +0200

    esquelet

---
+++ Cap4Avids/src/control/Main.java
@@ -0,0 +1,18 @@
+package control;
+
+public class Main implements Comunicar {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Envia un missatge
+     *
+     * @param s El missatge
+     */
+    @Override
+    public void comunicar(String s) {
+
+    }
+}
---
+++ Cap4Avids/src/vista/Finestra.java
@@ -0,0 +1,17 @@
+package vista;
+
+import control.Comunicar;
+
+import javax.swing.*;
+
+public class Finestra extends JFrame implements Comunicar {
+    /**
+     * Envia un missatge
+     *
+     * @param s El missatge
+     */
+    @Override
+    public void comunicar(String s) {
+
+    }
+}
