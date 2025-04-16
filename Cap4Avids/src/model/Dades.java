cce0b2a5cf94c83447a7c2c31c85579664a12f1a
Daniel GV <danielgv.space@gmail.com>
2025-04-16 21:13:56 +0200

    esquelet

--- Cap4Avids/src/model/Dades.java
+++ Cap4Avids/src/model/Dades.java
@@ -1,4 +1,12 @@
 package model;
 
+import control.Main;
+
 public class Dades {
+    private final Main principal;
+
+    public Dades(){
+        principal = Main.instance;
+    }
+
 }
--- Cap4Avids/src/vista/Finestra.java
+++ Cap4Avids/src/vista/Finestra.java
@@ -1,10 +1,22 @@
 package vista;
 
 import control.Comunicar;
+import control.Main;
+import model.Dades;
 
 import javax.swing.*;
 
 public class Finestra extends JFrame implements Comunicar {
+
+    private final Dades dades;
+    private final Comunicar principal;
+
+    public Finestra() {
+        principal = Main.instance;
+        dades = Main.instance.getDades();
+    }
+
+
     /**
      * Envia un missatge
      *
@@ -12,6 +24,9 @@ public class Finestra extends JFrame implements Comunicar {
      */
     @Override
     public void comunicar(String s) {
-
+        String[] args = s.split(":");
+        switch (args[0]) {
+            default -> System.err.println("WARNING: Finestra reb missatge?: " + s);
+        }
     }
 }
