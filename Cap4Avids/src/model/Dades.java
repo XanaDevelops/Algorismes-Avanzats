212adc0153589b41e874e9d1d11ac59edc9d600a
JoFeF08 <135724351+JoFeF08@users.noreply.github.com>
2025-04-18 16:45:49 +0200

    Incici del model amb una clase amb dues llistes

--- Cap4Avids/src/model/Dades.java
+++ Cap4Avids/src/model/Dades.java
@@ -2,11 +2,45 @@ package model;
 
 import control.Main;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dades {
     private final Main principal;
+
+    private final List<File> descomprimits = new ArrayList<>();
+    private final List<File> comprimits = new ArrayList<>();
 
     public Dades(){
         principal = Main.instance;
     }
 
+    public void addDescomprimit(File f){
+        descomprimits.add(f);
+    }
+    public void addComprimit(File f){
+        comprimits.add(f);
+    }
+
+    public void removeDescomprimit(File f){
+        descomprimits.remove(f);
+    }
+
+    public void removeComprimit(File f){
+        comprimits.remove(f);
+    }
+
+    public void clear(){
+        descomprimits.clear();
+        comprimits.clear();
+    }
+
+    public List<File> getComprimits() {
+        return comprimits;
+    }
+
+    public List<File> getDescomprimits() {
+        return descomprimits;
+    }
 }
