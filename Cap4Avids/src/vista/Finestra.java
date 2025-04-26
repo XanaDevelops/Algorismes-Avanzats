271c757a960743a118198be1a41757165e388d67
JoFeF08 <135724351+JoFeF08@users.noreply.github.com>
2025-04-26 13:31:46 +0200

    Update amb menu intermitj

--- Cap4Avids/src/vista/Finestra.java
+++ Cap4Avids/src/vista/Finestra.java
@@ -7,6 +7,7 @@ import model.Dades;
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
+import java.util.List;
 
 public class Finestra extends JFrame implements Comunicar {
 
@@ -17,9 +18,8 @@ public class Finestra extends JFrame implements Comunicar {
     private final PanellFitxers comprimits;
 
 
-    private final String[] nomsBtn = {"Carregar", "Comprimir", "Descomprimir", "Guardar"};
+    private final String[] nomsBtn = {"Carregar", "Comprimir", "Descomprimir", "Guardar", "Mostrar Arbre"};
     private final JButton[] botons = new JButton[nomsBtn.length];
-
 
 
     public Finestra() {
@@ -50,8 +50,8 @@ public class Finestra extends JFrame implements Comunicar {
         add(barra, BorderLayout.NORTH);
 
         // Panells inferiors: dos llistats amb drag & drop i botons add/remove
-        descomprimits = new PanellFitxers(this, "Arxius descomprimits", true ,dades);
-        comprimits = new PanellFitxers(this,"Arxius comprimits",false ,dades);
+        descomprimits = new PanellFitxers(this, "Arxius a comprimir", true, dades);
+        comprimits = new PanellFitxers(this, "Arxius a descomrmir comprimir", false, dades);
         JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, descomprimits, comprimits);
         split.setResizeWeight(0.5);
         add(split, BorderLayout.CENTER);
@@ -79,6 +79,27 @@ public class Finestra extends JFrame implements Comunicar {
                     principal.comunicar("Eliminar:" + f.getAbsolutePath());
                 }
             }
+            case "Comprimir" -> {
+                List<File> sel = descomprimits.getSelectedFiles();
+                DialegExecucio dlg = new DialegExecucio(this, DialegExecucio.Tipus.COMPRESS, sel);
+                String msg = dlg.mostra();
+                if (msg != null) Main.instance.comunicar(msg);
+            }
+            case "Descomprimir" -> {
+                List<File> sel = comprimits.getSelectedFiles();
+                DialegExecucio dlg = new DialegExecucio(this, DialegExecucio.Tipus.DECOMPRESS, sel);
+                String msg = dlg.mostra();
+                if (msg != null) Main.instance.comunicar(msg);
+            }
+
+            case "Guardar" -> Main.instance.comunicar("Guardar");
+
+            case "Mostrar Arbre" -> {
+                // FinestraArbre dlg = new FinestraArbre();
+                //dlg.mostra();
+                Main.instance.comunicar(nom);
+            }
+
             default -> {
                 // Comprimir, Descomprimir, Guardar
                 Main.instance.comunicar(nom);
@@ -100,7 +121,7 @@ public class Finestra extends JFrame implements Comunicar {
 
             switch (p[1]) {
                 case "descomprimit" -> descomprimits.refresh();
-                case "comprimit"   -> comprimits.refresh();
+                case "comprimit" -> comprimits.refresh();
                 default -> System.err.println(
                         "Finestra: no sé refrescar -> " + s);
             }
