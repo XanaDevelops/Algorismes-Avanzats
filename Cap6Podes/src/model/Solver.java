aec15136bca8b510334955efa558d13f2366876b
Ti-trit <khaoulaberrada395@gmail.com>
2025-06-09 20:20:09 +0200

    comentaris i test basic

--- Cap6Podes/src/model/Solver.java
+++ Cap6Podes/src/model/Solver.java
@@ -6,88 +6,54 @@ import controlador.Main;
 import java.util.*;
 
 public class Solver implements Runnable, Comunicar {
-    private static int totalNodes;
     private static final int INFINITY = Integer.MAX_VALUE;
-    private Node root;
-    private volatile static  boolean running = true;
+    private volatile static boolean running = true;
     private volatile static boolean paused = false;
+    private int totalNodes;
+    private Node root;
     private int[][] graf;
+    private boolean modeAll = true;
     public Solver(int id, int n) throws InterruptedException {
         Random r = new Random();
-        int [][] matriu = new int[n][n];
+        int[][] matriu = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                 if (i == j) {
-                     matriu[i][j] = INFINITY;
-                 }else{
-                     matriu[i][j] = r.nextInt(100);
-                 }
+                if (i == j) {
+                    matriu[i][j] = INFINITY;
+                } else {
+                    matriu[i][j] = r.nextInt(100);
+                }
 
             }
         }
         this.graf = matriu;
         totalNodes = graf.length;
-
     }
-    public Solver (int id, int[][] matrix) throws InterruptedException {
+
+    public Solver(int id, int[][] matrix) throws InterruptedException {
         this.graf = matrix;
         totalNodes = graf.length;
     }
 
-    public Node getRoot() {
-        return root;
+    private static int trobarMinFila(int[][] mat, int i) {
+        int min = INFINITY;
+        for (int j = 0; j < mat.length; j++) {
+            if (mat[i][j] < min) min = mat[i][j];
+        }
+        return min;
     }
-
-
 
     @Override
     public void run() {
         try {
             root = solve(graf);
+
             obtenirSolucio(root);
+            System.out.println(root.cost);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-    }
-
-
-    public static class Node implements Comparable<Node> {
-        int[][] reducedMatrix;
-        int current;            //vertex del node actual
-        int cost;               //cost acumulat fins el node actual
-        Node parent;
-        int depth;
-
-        Node(int[][] reducedMatrix, int current, int cost, Node parent, int depth) {
-            this.reducedMatrix = reducedMatrix;
-            this.current = current;
-            this.cost = cost;
-            this.parent = parent;
-            this.depth = depth;
-
-        }
-
-        public int getCurrent() {
-            return current;
-        }
-
-        public void setCurrent(int current) {
-            this.current = current;
-        }
-
-        public Node getParent() {
-            return parent;
-        }
-
-        public void setParent(Node parent) {
-            this.parent = parent;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return Integer.compare(this.cost, o.cost);
-        }
     }
 
     Node solve(int[][] costMatrix) throws InterruptedException {
@@ -101,23 +67,23 @@ public class Solver implements Runnable, Comunicar {
 
         //BB basat en triar el node més prometedor
         while (!pq.isEmpty() && running) {
-//            synchronized (this) {
-//                while (paused) {
-//                    wait();  // esperar fins que es crida a resume()
-//                }
-//            }
+            synchronized (this) {
+                while (paused) {
+                    wait();  // esperar fins que es crida a resume()
+                }
+            }
             Node node = pq.poll();
             //hem visitat tots els nodes?
-            if (Objects.requireNonNull(node).depth == totalNodes - 1) {
-                int returnCost = node.reducedMatrix[node.current][0];
-                node.cost +=  returnCost == INFINITY ? 0: INFINITY;
+
+            if (Objects.requireNonNull(node).profund == totalNodes - 1) {
+                int returnCost = node.matriuReduida[node.actual][0];
+                node.cost += returnCost != INFINITY ? returnCost : 0;
                 return node;
             }
 
-
             for (int j = 0; j < totalNodes; j++) {
-                if (node.reducedMatrix[node.current][j] != INFINITY) {
-                    pq.add(createChild(node, j));
+                if (node.matriuReduida[node.actual][j] != INFINITY) {
+                    pq.add(crearNodeFill(node, j));
                 }
             }
         }
@@ -125,47 +91,39 @@ public class Solver implements Runnable, Comunicar {
     }
 
 
-
-    static Node createChild(Node parent, int nextVertex) {
-        int[][] matriu = deepCopy(parent.reducedMatrix);
-        int current = parent.current;
+    private Node crearNodeFill(Node parent, int nextVertex) {
+        int[][] matriu = deepCopy(parent.matriuReduida);
+        int current = parent.actual;
         // Marcar fila y columna amb INFINTY per eliminar camins ja visitats.
         for (int k = 0; k < totalNodes; k++) {
             matriu[current][k] = INFINITY;
             matriu[k][nextVertex] = INFINITY;
         }
-        matriu[nextVertex][0] = INFINITY;
 
-        int addedCost = parent.reducedMatrix[current][nextVertex];
-        int reductionCost = calcularCostReduccio(matriu);
-        return new Node(matriu, nextVertex, parent.cost + addedCost + reductionCost, parent, parent.depth + 1);
+        int costAfegit = parent.matriuReduida[current][nextVertex];
+        int costReduccio = calcularCostReduccio(matriu);
+        return new Node(matriu, nextVertex, parent.cost + costAfegit + costReduccio, parent, parent.profund + 1);
     }
 
     private void obtenirSolucio(Node leaf) {
-        LinkedList<Integer> path = new LinkedList<>();
+        LinkedList<Integer> cami = new LinkedList<>();
         while (leaf != null) {
-            path.addFirst(leaf.current);
-            leaf = leaf.parent;
+            cami.addFirst(leaf.actual);
+            leaf = leaf.pare;
         }
-//        for (Integer integer : path) {
-//            System.out.println(integer);
-//        }
-        Main.getInstance().getDades().guardarSolucio(path);
 
+        Main.getInstance().getDades().guardarSolucio(cami);
     }
 
-
-
-    static int[][] deepCopy(int[][] m) {
-        int[][] c = new int[totalNodes][totalNodes];
-        for (int i = 0; i < totalNodes; i++) {
-            System.arraycopy(m[i], 0, c[i], 0, totalNodes);
+    private int[][] deepCopy(int[][] m) {
+        int[][] c = new int[m.length][]; // Copia solo referencia a filas
+        for (int i = 0; i < m.length; i++) {
+            c[i] = Arrays.copyOf(m[i], m[i].length); // Copia solo contenido de cada fila
         }
         return c;
     }
 
-
-    static int calcularCostReduccio(int[][] mat) {
+    private int calcularCostReduccio(int[][] mat) {
         int cost = 0;
         // reduir files
         for (int i = 0; i < mat.length; i++) {
@@ -179,7 +137,6 @@ public class Solver implements Runnable, Comunicar {
 
                 }
             }
-
         }
 
         //reduir columnes
@@ -199,32 +156,21 @@ public class Solver implements Runnable, Comunicar {
         return cost;
     }
 
-    private static int trobatMinColumna(int[][] mat, int i) {
+    private int trobatMinColumna(int[][] mat, int i) {
         int min = INFINITY;
 
         for (int[] ints : mat) {
             if (ints[i] < min) min = ints[i];
-
         }
         return min;
     }
-
-    private static int trobarMinFila(int[][] mat, int i) {
-        int min = INFINITY;
-        for (int j = 0; j < mat.length; j++) {
-            if (mat[i][j] < min) min = mat[i][j];
-        }
-        return min;
-    }
-
-
 
     public void interrompre() {
         running = false;
     }
 
     public void resume() {
-        paused= false;
+        paused = false;
         synchronized (this) {
             notify();
         }
@@ -238,5 +184,26 @@ public class Solver implements Runnable, Comunicar {
     @Override
     public void comunicar(String msg) {
 
+    }
+
+    public static class Node implements Comparable<Node> {
+        int[][] matriuReduida;
+        int actual;            //vertex del node actual
+        int cost;               //cost acumulat fins el node actual
+        Node pare;
+        int profund;
+
+        Node(int[][] reducedMatrix, int actual, int cost, Node pare, int profund) {
+            this.matriuReduida = reducedMatrix;
+            this.actual = actual;
+            this.cost = cost;
+            this.pare = pare;
+            this.profund = profund;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.cost, o.cost);
+        }
     }
 }
