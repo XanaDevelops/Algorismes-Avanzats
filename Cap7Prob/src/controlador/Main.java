8310337811ab9d817433b4fa8aa3428adf2dd684
bielperello <biel.perello1@estudiant.uib.cat>
2025-06-17 13:16:34 +0200

    Esquelet controlador

--- Cap7Prob/src/controlador/Main.java
+++ Cap7Prob/src/controlador/Main.java
@@ -9,5 +9,39 @@ import java.util.*;
 import java.util.concurrent.*;
 
 public class Main implements Comunicar {
+    private static Main instance;
 
+    public static Main getInstance() {
+        return instance;
+    }
+
+    private Dades dades;
+    private Comunicar finestra;
+
+    public Main(){
+        if (instance == null) {
+            instance = this;
+        } else {
+            return;
+        }
+
+        dades = new Dades();
+    }
+
+    public static void main(String[] args) {
+        new Main().init();
+    }
+
+    private void init() {
+        SwingUtilities.invokeLater(() -> {
+            finestra = new Finestra();
+        });
+    }
+
+    @Override
+    public void comunicar(String msg) {
+        String[] args = msg.split(":");
+
+        System.err.println("MAIN: missatge? " + msg);
+    }
 }
