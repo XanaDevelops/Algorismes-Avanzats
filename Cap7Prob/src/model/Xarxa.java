1fcfa096d6be8a229093eecae5cc77773e090426
Daniel GV <danielgv.space@gmail.com>
2025-06-18 01:14:15 +0200

    save

--- Cap7Prob/src/model/Xarxa.java
+++ Cap7Prob/src/model/Xarxa.java
@@ -38,6 +38,70 @@ public class Xarxa extends Solver implements Serializable {
         this(nEntrades, config, nSortides, 0.01);
     }
 
+    public double[] predecir(double[] entrada){
+        assert entrada.length == nEntrades;
+
+        //entrada a capa oculta
+        for (int i = 0; i < capesOcultes[0].length; i++) {
+            double suma = 0;
+            for (int j = 0; j < nEntrades; j++) {
+                suma += pesosEntradaOculta[i][j] * entrada[j];
+            }
+            capesOcultes[0][i] = sigmoide(suma);
+        }
+
+        //capes ocultes
+        for(int i = 0; i < pesosOcultes.length; i++){
+            for (int j = 0; j < capesOcultes[i+1].length; j++) {
+                double suma = 0;
+                for (int k = 0; k < capesOcultes[i].length; k++) {
+                    suma += pesosOcultes[i][j][k];
+                }
+                capesOcultes[i+1][j] = sigmoide(suma);
+            }
+        }
+
+        double[] sumesSortida = new double[nSortides];
+        for (int i = 0; i < sumesSortida.length; i++) {
+            double suma = 0;
+            for (int j = 0; j < pesosSortida[i].length; j++) {
+                suma += pesosSortida[i][j] * capesOcultes[capesOcultes.length-1][j];
+            }
+            sumesSortida[i] = sigmoide(suma);
+        }
+
+        return sumesSortida;
+    }
+
+    public void entrenar(double[][] entrades, double[][] sortides, int epoc){
+        assert entrades[0].length == nEntrades;
+        for (int _i = 0; _i < epoc; _i++) {
+            double errorTotal = 0;
+            for (int i = 0; i < entrades.length; i++) {
+                double[] sortida = predecir(entrades[i]);
+                double[] error = new double[nSortides];
+                for (int j = 0; j < error.length; j++) {
+                    error[j] = sortides[i][j] - sortida[j];
+                }
+                double[] deltaSortida = new double[nSortides];
+                for (int j = 0; j < deltaSortida.length; j++) {
+                    deltaSortida[j] = error[j] * derivadaSigmoide(sortida[j]);
+                }
+
+                double[][] deltaOcultes = new double[pesosOcultes.length][];
+
+
+                //backp oculta -> sortides
+                for (int j = 0; j < pesosSortida.length; j++) {
+                    for (int k = 0; k < pesosSortida[j].length; k++) {
+                        pesosSortida[j][k] = deltaEntrenament * deltaSortida[j] * capesOcultes[capesOcultes.length-1][k];
+                    }
+                }
+
+
+            }
+        }
+    }
 
     // Función Sigmoide (para la salida)
     private double sigmoide(double x) {
